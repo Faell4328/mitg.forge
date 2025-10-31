a695445784db4690be069b124e6e8e157de334c1
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2025-10-31 14:04:49 -0400

    feat: add footer text

--- apps/web/src/layout/Footer/index.tsx
+++ apps/web/src/layout/Footer/index.tsx
@@ -1,12 +1,27 @@
 export const Footer = () => {
 	return (
 		<footer
-			className="min-h-[60px] font-fondamento"
+			className="mt-5 min-h-[60px] font-fondamento"
 			style={{
 				gridArea: "footer",
 			}}
 		>
-			footer content
+			<div className="flex flex-col items-center font-roboto text-white text-xs">
+				<span>Copyright by MITG. All rights reserved.</span>
+				<div className="flex flex-row gap-1">
+					<a href="/#" className="hover:underline">
+						About MITG
+					</a>
+					<span> | </span>
+					<a href="/#" className="hover:underline">
+						Service Agreement
+					</a>
+					<span> | </span>
+					<a href="/#" className="hover:underline">
+						Privacy Policy
+					</a>
+				</div>
+			</div>
 		</footer>
 	);
 };
